2ff33459f615262ced6f58b9f70b2be293044bcf
margo <margo.bozova@gmail.com>
2017-12-19 15:40:57 +0200

    edit server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/get-from-object/:name', (req, res) => {
+  console.log(req.params.name);
+  res.send(req.params.name);
+});
+
+app.post('/set-in-object', (req, res) => {
+  res.send(req.body);
+});
 
 export default app;
---
+++ volumes/conf.d
@@ -0,0 +1,22 @@
+server {
+  listen 3008;
+  server_name localhost;
+
+  location /set-in-object {
+    proxy_pass http://second:3000/set;
+    proxy_set_header Host $host;
+    proxy_set_header X-Real-IP $remote_addr;
+    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    proxy_set_header X-Forwarded-Proto $scheme;
+    proxy_redirect off;
+  }
+
+  location /get-from-object/name {
+    proxy_pass http://second:3000/get;
+    proxy_set_header Host $host;
+    proxy_set_header X-Real-IP $remote_addr;
+    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    proxy_set_header X-Forwarded-Proto $scheme;
+    proxy_redirect off;
+  }
+}
